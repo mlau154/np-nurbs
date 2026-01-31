a33e6046c1e3b70a06690bff1a5a7ab215f6bda8
mlau154 <mlauer2015@gmail.com>
2026-01-31 10:38:33 -0700

    Added bezier_curve_anyderiv_grid function that is now the backend for all bezier_curve_grid functions

--- np_nurbs/bezier.py
+++ np_nurbs/bezier.py
@@ -4,11 +4,76 @@ from np_nurbs import coefficient_matrices
 
 
 __all__ = [
+    "bezier_curve_anyderiv_grid",
     "bezier_curve_eval_grid",
     "bezier_curve_dcdt_grid",
     "bezier_curve_d2cdt2_grid",
     "bezier_surf_eval_grid",
 ]
+
+
+def bezier_curve_anyderiv_grid(
+        p: np.ndarray[tuple[int, int], np.dtype[float]],
+        nt: int,
+        deriv_order: int,
+        ) -> np.ndarray[tuple[int, int], np.dtype[float]]:
+    """
+    Evaluates a Bézier curve derivative of any order (including
+    0, which implies a pure curve evaluation) 
+    on an evenly spaced parameter vector
+    (``linspace(0, 1, nt)``) using a fully vectorized formulation.
+
+    Parameters
+    ----------
+    p: np.ndarray[tuple[int, int], np.dtype[float]]
+        Bézier control point array. This array has shape
+        :math:`(n+1) \\times d`, where :math:`n` is
+        the curve degree and :math:`d` is the number
+        of dimensions (usually 2 or 3)
+    nt: int
+        Number of evenly spaced parameters at which to
+        evaluate the curve derivative
+    deriv_order: int
+        Order of the derivative to evaluate.
+
+        - Must be a postive integer
+        - If 0, the shape of the curve is returned
+        - If the order of the derivative is less than
+          the curve degree, the derivative is zero
+          everywhere (an array of zeros is immediately
+          returned in this case)
+
+    Returns
+    -------
+    np.ndarray[tuple[int, int], np.dtype[float]]
+        The evaluated Bézier curve with shape
+        :math:`n_t \\times d`, where :math:`n_t`
+        is the number of parameters
+    """
+    # Get the degree and determine early if the derivative returns zero
+    degree = len(p) - 1
+    if deriv_order >= degree:
+        return np.zeros(shape=(nt, p.shape[1]))
+
+    # Configure t matrix
+    t = np.linspace(0.0, 1.0, nt, dtype=float)
+    powers = (degree - deriv_order - np.arange(
+        degree + 1 - deriv_order))[:, np.newaxis]
+    t_mat = t ** powers
+    
+    # Grab the Pascal's triangle coefficient matrix from the stored hashmap
+    m = coefficient_matrices[degree - deriv_order]
+
+    # Get the control point or control point difference matrix
+    p_diff = p if deriv_order == 0 else np.diff(p, n=deriv_order, axis=0)
+    
+    # Compute the product
+    degree_product = 1 if deriv_order == 0 else np.prod(np.arange(
+        degree, degree - deriv_order, -1))
+    a = np.dot(t_mat.T, m)
+    b = degree_product * np.dot(a, p_diff)
+    
+    return b
 
 
 def bezier_curve_eval_grid(
@@ -37,17 +102,7 @@ def bezier_curve_eval_grid(
         :math:`n_t \\times d`, where :math:`n_t`
         is the number of parameters
     """
-    degree = len(p) - 1
-    t = np.linspace(0.0, 1.0, nt, dtype=float)
-    powers = (degree - np.arange(degree + 1))[:, np.newaxis]
-    t_mat = t ** powers
-    
-    m = coefficient_matrices[degree]
-    
-    a = np.dot(t_mat.T, m)
-    b = np.dot(a, p)
-    
-    return b
+    return bezier_curve_anyderiv_grid(p, nt, 0)
 
 
 def bezier_curve_dcdt_grid(
@@ -78,19 +133,7 @@ def bezier_curve_dcdt_grid(
         :math:`n_t \\times d`, where :math:`n_t`
         is the number of parameters
     """
-    degree = len(p) - 1
-    t = np.linspace(0.0, 1.0, nt, dtype=float)
-    powers = (degree - 1 - np.arange(degree + 1 - 1))[:, np.newaxis]
-    t_mat = t ** powers
-    
-    m = coefficient_matrices[degree - 1]
-
-    p_diff = np.diff(p, n=1, axis=0)
-    
-    a = np.dot(t_mat.T, m)
-    b = degree * np.dot(a, p_diff)
-    
-    return b
+    return bezier_curve_anyderiv_grid(p, nt, 1) 
 
 
 def bezier_curve_d2cdt2_grid(
@@ -121,19 +164,7 @@ def bezier_curve_d2cdt2_grid(
         :math:`n_t \\times d`, where :math:`n_t`
         is the number of parameters
     """
-    degree = len(p) - 1
-    t = np.linspace(0.0, 1.0, nt, dtype=float)
-    powers = (degree - 2 - np.arange(degree + 1 - 2))[:, np.newaxis]
-    t_mat = t ** powers
-    
-    m = coefficient_matrices[degree - 2]
-
-    p_diff = np.diff(p, n=2, axis=0)
-    
-    a = np.dot(t_mat.T, m)
-    b = degree * (degree - 1) * np.dot(a, p_diff)
-    
-    return b
+    return bezier_curve_anyderiv_grid(p, nt, 2)
 
 
 def bezier_surf_eval_grid(
